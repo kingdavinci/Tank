24db81554fc6f3ff6696ec36768679993611fcb4
Andrew Nelson <20.andrew.nelson@ksd.org>
2019-01-31 09:31:52 -0800

    Redo UI

---
+++ TankProject/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 velocity = new Vector2(x, y);
+        GetComponent<Rigidbody2D>().velocity = velocity * speed;
+
+	}
+}
