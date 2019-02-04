8b3b44fd752ab46fee1875c3fc39e494837db75c
Andrew Nelson <20.andrew.nelson@ksd.org>
2019-02-04 08:44:34 -0800

    EnemyChase script

---
+++ TankProject/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerShoot : MonoBehaviour {
+    public Slider WaitTimeShoot;
+    public GameObject prefab;
+    public float shootSpeed = 0;
+    float timer = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        WaitTimeShoot.GetComponent<Slider>().value = timer;
+		if(Input.GetButton("Fire1") && timer > 0.2f)
+        {
+            timer = 0;
+            var mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition.z = -10;
+            // when calculating a vector from point a to b , always do destination - start position
+            Vector3 shootDir = mousePosition - transform.position;
+            shootDir.Normalize();
+            Vector3 offset = shootDir;
+            shootDir = shootDir * shootSpeed;
+            GameObject bullet = (GameObject)Instantiate(prefab,
+                transform.position + offset, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shootDir;
+            Destroy(bullet, 0.5f);
+        }
+	}
+}
