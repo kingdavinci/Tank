b016f15ca920034f2bf9368e342d82c5c4c1ddb7
Kent Melville <19.kent.melville@ksd.org>
2019-02-20 09:19:07 -0800

    MedKit, which hopefully will move after this

---
+++ TankProject/Assets/Scripts/MedKit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : MonoBehaviour
+{
+    public Vector3 paceDirection = new Vector3(0, 0, 0);
+    public float paceDistance = 3.0f;
+    public float paceSpeed = 2.0f;
+    public float health = 10;
+    public GameObject Player;
+    Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 displacement = transform.position - startPosition;
+        float distanceFromStart = displacement.magnitude;
+        if (distanceFromStart >= paceDistance)
+        {
+            // flip directions
+            paceDirection = -paceDirection;
+        }
+        paceDirection.Normalize();
+        GetComponent<Rigidbody2D>().velocity = paceDirection * paceSpeed;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Player.GetComponent<PlayerHP>().hp += 5;
+            Destroy(collision.gameObject);
+        }
+    }
+}
