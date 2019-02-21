36c2065926a442370d55da96f1719c8260cfdbd7
Kent Melville <19.kent.melville@ksd.org>
2019-02-21 08:44:38 -0800

    made main menu script, & attached to button

---
+++ TankProject/Assets/Scripts/LoadMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
