7589baf31639f2c85ad324efd8ad185c3b733021
Kent Melville <19.kent.melville@ksd.org>
2019-03-07 10:21:28 -0800

    created boss, boss tigger/hp script, adjusted level 4 level design (?), created boss hp slider/text & timer

---
+++ TankProject/Assets/Scripts/BossHP.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BossHP : MonoBehaviour
+{
+    public float hp = 30;
+    public GameObject healthBar;
+    public GameObject healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.GetComponent<Slider>().value = hp;
+        healthText.GetComponent<Text>().text = "BossHP: " + hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     //   healthBar.GetComponent<Slider>().value = hp;
+       // healthText.GetComponent<Text>().text = "BossHP: " + hp;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("Win");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            hp -= 1;
+            healthText.GetComponent<Text>().text = "BossHP: " + hp;
+            healthBar.GetComponent<Slider>().value = hp;
+        }
+    }
+}
---
+++ TankProject/Assets/Scripts/BossTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class BossTrigger : MonoBehaviour
+{
+    public float timer = 90;
+   // public GameObject BossHP;
+   // public GameObject BossHPText;
+    bool boss = false;
+    public GameObject Boss;
+    public Text time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Help");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boss == true)
+        {
+            timer -= Time.deltaTime;
+            Boss.GetComponent<BossHP>().healthBar.SetActive(true);
+            Boss.GetComponent<BossHP>().healthText.SetActive(true);
+            time.GetComponent<Text>().enabled = true;
+            time.GetComponent<Text>().text = "Time: " + timer;
+        }
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("Lose");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            boss = true;
+            Debug.Log("hi");
+        }
+    }
+}
